81c9da153d0c33535e37e9fdcbb9a9bcdc9dc36a
artdeman <56304585+artdeman@users.noreply.github.com>
2022-11-03 19:11:05 +0100

    Add files via upload

---
+++ onepage.js
@@ -0,0 +1,59 @@
+
+
+//animation parallax//
+
+     var controller = new ScrollMagic.Controller();
+
+     var scene = new ScrollMagic.Scene({
+
+         triggerElement: '.box0',
+         reverse: false
+     })
+
+         .setClassToggle('.box0', 'fade-in')
+         .addTo(controller);
+
+     var scene1 = new ScrollMagic.Scene({
+
+         triggerElement: '.box1',
+         reverse: false
+     })
+
+         .setClassToggle('.box1', 'fade-in')
+         .addTo(controller);
+
+     var scene2 = new ScrollMagic.Scene({
+
+         triggerElement: '.box2',
+         reverse: false
+     })
+
+         .setClassToggle('.box2', 'fade-in')
+         .addTo(controller);
+
+     var scene3 = new ScrollMagic.Scene({
+
+         triggerElement: '.box3',
+         reverse: false
+     })
+
+         .setClassToggle('.box3', 'fade-in')
+         .addTo(controller);
+
+    var scene4 = new ScrollMagic.Scene({
+
+        triggerElement: '.box4',
+        reverse: false
+    })
+
+          .setClassToggle('.box4', 'fade-in')
+          .addTo(controller);
+
+      var scene5 = new ScrollMagic.Scene({
+
+         triggerElement: '.box5',
+         reverse: false
+      })
+
+      .setClassToggle('.box5', 'fade-in')
+      .addTo(controller);
